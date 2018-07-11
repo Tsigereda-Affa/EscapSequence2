42d5b125851daf66813212cb806e8d8f98ad3b40
tsigereda-affa <affa16.a@gmail.com>
2018-07-11 17:21:42 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args){
+       //Write a Java program to print the following columns of values. Use \t for the tabs between the columns.
+        System.out.println("Cost\t\t\tQuality\t\tTotal");
+        System.out.println("$1,000.00 \t\t4\t\t$4,000.00");
+        System.out.println("$\t50.00 \t\t8\t\t  $400.00");
+        System.out.println("Total:\t\t\t12\t\t$4,400.00");
+
+    }
+}
